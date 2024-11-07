1b4c68ef437c2c7f3969c6ed3833ff03488c0df8
igoreor <igoreor24@gmail.com>
2024-11-07 09:28:50 -0300

    feat(TaskList): adicionar o TaskList

---
+++ src/components/TaskFilter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <select v-model="selectedCategory">
+        <option value="">All categories</option>
+        <option value="work">Work</option>
+        <option value="personal">Personal</option>
+      </select>
+  
+      <select v-model="selectedStatus">
+        <option value="all">All</option>
+        <option value="completed">Completed</option>
+        <option value="pending">Pending</option>
+      </select>
+    </div>
+  </template>
+  
+  <script>
+  import { useTaskStore } from '@/stores/taskStore';
+  import { ref, watch } from 'vue';
+  
+  export default {
+    setup() {
+      const taskStore = useTaskStore();
+      const selectedCategory = ref('');
+      const selectedStatus = ref('all');
+  
+      watch([selectedCategory, selectedStatus], () => {
+      });
+  
+      return { selectedCategory, selectedStatus };
+    }
+  };
+  </script>
+  
