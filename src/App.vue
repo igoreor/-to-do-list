defac35deea3117a30e25a1ccfd76ff573e7706f
igoreor <igoreor24@gmail.com>
2024-11-07 09:34:07 -0300

    configurações adicionais

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,31 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div id="app">
+      <h1>Lista de Tarefas Avançada</h1>
+    
+      <TaskFilter />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <TaskForm />
+  
+      <TaskList />
+        
+      <TaskStats />
     </div>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+  </template>
+  
+  <script>
+  import TaskList from './components/TaskList.vue';
+  import TaskForm from './components/TaskForm.vue';
+  import TaskFilter from './components/TaskFilter.vue';
+  import TaskStats from './components/TaskStats.vue';
+  
+  
+  export default {
+    components: {
+      TaskList,
+      TaskForm,
+      TaskFilter,
+      TaskStats,
+    },
+  };
+  </script>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// src/router/index.js
+import { createRouter, createWebHistory } from "vue-router";
+import TaskList from "../components/TaskList.vue";
+import TaskForm from "../components/TaskForm.vue";
+
+const routes = [
+  { path: "/", component: TaskList }, // Rota para lista de tarefas
+  { path: "/add-task", component: TaskForm }, // Rota para adicionar uma tarefa
+  // Adicione outras rotas conforme necessário
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
