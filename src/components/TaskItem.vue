bdfca49f3e55aec04e2fb10f6fd6db970d06686d
igoreor <igoreor24@gmail.com>
2024-11-07 09:30:24 -0300

    feat(TaskFilter): adicionar o TaskFilter

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="task-item" :class="{ 'completed-task': task.completed }">
+      <input type="checkbox" class="task-checkbox" v-model="task.completed" @change="toggleStatus(task.id)" />
+      <span class="task-title">{{ task.title }}</span>
+      <button @click="editTask(task.id)">Edit</button>
+      <button @click="removeTask(task.id)">Delete</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      task: Object
+    },
+    emits: ['toggle', 'edit', 'remove'],
+    methods: {
+      toggleStatus(id) {
+        this.$emit('toggle', id);
+      },
+      editTask(id) {
+        this.$emit('edit', id);
+      },
+      removeTask(id) {
+        this.$emit('remove', id);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .task-item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+  }
+  
+  .task-checkbox {
+   
+    margin-top: 20px; 
+  }
+  
+  .task-title {
+    flex: 1;
+    margin-right: 10px;
+  }
+  
+  button {
+    margin-right: 5px;
+  }
+  
+  .completed-task {
+    text-decoration: line-through;
+  }
+  </style>
+  
