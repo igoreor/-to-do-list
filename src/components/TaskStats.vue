c41567f5e3bca77885af757be9b42af3554e8b83
igoreor <igoreor24@gmail.com>
2024-11-07 09:32:49 -0300

    feat(TaskeStats): adicionar o TaskStats

---
+++ src/components/TaskStats.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{ completedCount }}</p>
+    <p>Pending Tasks: {{ pendingCount }}</p>
+  </div>
+</template>
+
+<script>
+import { useTaskStore } from '@/stores/taskStore';
+import { computed } from 'vue';
+
+export default {
+  setup() {
+    const taskStore = useTaskStore();
+    
+  
+    const completedCount = computed(() => taskStore.completedTasksCount.value);
+    const pendingCount = computed(() => taskStore.pendingTasksCount.value);
+
+    return { completedCount, pendingCount };
+  }
+};
+</script>
